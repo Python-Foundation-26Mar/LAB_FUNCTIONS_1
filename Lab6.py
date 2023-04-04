9f483e1c3979c6c165fd50443cd06df0f8ca6a17
Manar Alharbi <manar.alharbi2953@gmail.com>
2023-04-04 12:34:00 +0300

    Lab6

---
+++ Lab6.py
@@ -0,0 +1,31 @@
+for numbers in range(1,2):
+    print(1,2,3,4,5 , end=" ")
+    print(" ")
+
+for numbers in range(1,2):
+    print(1,2,3,4, end=" ")
+    print(" ")
+    
+for numbers in range(1,2):
+    print(1,2,3 , end=" ")
+    print(" ")
+
+for numbers in range(1,2):
+    print(1,2 , end=" ")
+    print(" ")
+
+for numbers in range(1,2):
+    print(1 , end=" ")
+    print(" ")
+    
+
+ 
+
+
+
+
+
+def calculate_bmi(weight:int, height:float):
+    ''' a function to calculate the body mass index (bmi) and returns it, it takes a weight and a height as parameters '''
+    print("Documentatio of the function calculate_bmi")
+    print(calculate_bmi.__doc__)
